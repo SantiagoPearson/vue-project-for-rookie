746c909b4f587b4f127d24d243414f214028ab45
lhw <2251360348@qq.com>
2020-03-31 22:53:37 +0800

    waimai

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+      <router-view/>
+      <FooterGuide v-show="$route.meta.showFooter"/>
+    </div>
+</template>
+
+<script>
+  import {mapActions} from 'vuex'
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  import { Button } from 'mint-ui';
+  import Vue from 'vue'
+
+  Vue.component(Button.name, Button);
+    export default {
+      mounted () {
+        // this.$store.dispatch('getAddress')
+        this.getAddress()
+        // this.getUserInfo()
+        this.$store.dispatch('getUserInfo')
+      },
+
+      methods: {
+        ...mapActions(['getAddress', 'getUserInfo'])
+      },
+
+      components: {
+        FooterGuide
+      }
+
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,83 @@
+/*
+路由器对象模块
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
+import Shop from "../pages/Shop/Shop";
+import ShopGoods from "../pages/Shop/ShopGoods/ShopGoods";
+import ShopInfo from "../pages/Shop/ShopInfo/ShopInfo";
+import ShopRatings from "../pages/Shop/ShopRatings/ShopRatings";
+
+// 声明使用插件
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  // 所有路由
+  routes: [
+    {
+      path: '/msite',
+      component: MSite, // 返回路由组件的函数, 只有执行此函数才会加载路由组件, 这个函数在请求对应的路由路径时才会执行
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/shop',
+      component: Shop,
+      children: [
+        {
+          path: '/shop/goods',
+          component: ShopGoods
+        },
+        {
+          path: '/shop/ratings',
+          component: ShopRatings
+        },
+        {
+          path: '/shop/info',
+          component: ShopInfo
+        },
+        {
+          path: '',
+          redirect: '/shop/goods'
+        },
+      ]
+    },
+  ]
+})
